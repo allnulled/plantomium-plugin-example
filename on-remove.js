6735a713d2a629898f4186d8c395b35534b683c5
allnulled <todosanulados@gmail.com>
2020-09-13 00:09:15 +0200

    fixed method name

--- on-remove.js
+++ on-remove.js
@@ -2,7 +2,7 @@ module.exports = async function() {
 	try {
 		const fs = require("fs-extra");
 		const fileprint = process.env.PROJECT_ROOT + "/docs/plugins/plantomium/example/README.md";
-		fs.removeFileSync(fileprint);
+		fs.removeSync(fileprint);
 	} catch(error) {
 		throw error;
 	}
