31a0de0753c60f0e2f5c3aa7f89eb9b78b71c230
allnulled <todosanulados@gmail.com>
2020-09-12 23:59:47 +0200

    changed fileprint to docs

--- on-remove-plugin.js
+++ on-remove-plugin.js
@@ -1,8 +1,8 @@
 module.exports = async function() {
 	try {
 		const fs = require("fs");
-		const fileprint = process.env.PROJECT_ROOT + "/src/plugins/plantomium.example.txt";
-		fs.unlinkFileSync(fileprint);
+		const fileprint = process.env.PROJECT_ROOT + "/docs/plugins/plantomium/example/README.md";
+		fs.removeFileSync(fileprint);
 	} catch(error) {
 		throw error;
 	}
