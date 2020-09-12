a8d2e3e8b843357bdfb5eb8a1f6403a2c4ab1e77
allnulled <todosanulados@gmail.com>
2020-09-13 00:07:46 +0200

    fixed dependency

--- on-remove.js
+++ on-remove.js
@@ -1,6 +1,6 @@
 module.exports = async function() {
 	try {
-		const fs = require("fs");
+		const fs = require("fs-extra");
 		const fileprint = process.env.PROJECT_ROOT + "/docs/plugins/plantomium/example/README.md";
 		fs.removeFileSync(fileprint);
 	} catch(error) {
